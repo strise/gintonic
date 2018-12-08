c772ce6acaadd94377e8ffedfa7f4df8488fc7e4
Christian Budde Christensen <budde377@gmail.com>
2018-12-08 11:52:16 +0100

    Fix script paths

--- packages/gqltrans-api/bin/cli.sh
+++ packages/gqltrans-api/bin/cli.sh
@@ -1,8 +1,14 @@
 #!/usr/bin/env bash
+SOURCE="${BASH_SOURCE[0]}"
+while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
+  DIR="$( cd -P "$( dirname "$SOURCE" )" >/dev/null && pwd )"
+  SOURCE="$(readlink "$SOURCE")"
+  [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+done
+DIR="$( cd -P "$( dirname "$SOURCE" )" >/dev/null && pwd )"
 
-parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
-script="${parent_path}/../src"
-configDir="${parent_path}/../config"
+script="${DIR}/../src"
+configDir="${DIR}/../config"
 if [[ -z "${MAX_STACK_SIZE}" ]]; then
   NODE_CONFIG_DIR="${configDir}" node "$script"
 else
